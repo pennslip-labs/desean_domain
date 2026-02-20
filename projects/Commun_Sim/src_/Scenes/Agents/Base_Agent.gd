ae6939893c8077a0c55d42aa7c73332322a99de8
slipnut44 Sham <shamarpennant@gmail.com>
2026-02-19 23:52:43 -0500

    created a basic working agent, as well as some scripts for the buildings

--- projects/Commun_Sim/src_/Scenes/Agents/Base_Agent.gd
+++ projects/Commun_Sim/src_/Scenes/Agents/Base_Agent.gd
@@ -5,8 +5,9 @@ class_name Base_Agent
 # constsant to be used in case of checking the current state
 const idle = 'i'
 const enroute = 'e'
+const locked = 'l' # specifically used when engaging with an event
 
-var speed := 50
+var speed := 0.75
 var state := idle # i for idle, e for enroute
 var target_position := Vector2i()
 
@@ -18,12 +19,15 @@ func move_to(target: Vector2i):
 # basic means of sprite movement towards target location
 func _physics_process(delta: float) -> void:
 	if state == enroute:
-		var direction = (target_position - snapped(global_position, 1))
+		var direction = (target_position - Vector2i(global_position.x, global_position.y))
 		velocity = direction * speed # uses a built in velocity variable
 		move_and_slide() # built in movement method
 	
-	# once the agent has reached within a certain range of the target posi
+	# once the agent has reached within a certain range of the target posi,
+	# consider the target posi reached and return the agent back to idle
+	# to stop the agent from overshooting their target
 	if global_position.distance_to(target_position) < 4:
 		state = idle
 		velocity = Vector2i.ZERO
 	pass
+	
