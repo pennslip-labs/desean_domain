81055b7a5804bb95fae9a300560df694940156e4
slipnut44 Sham <shamarpennant@gmail.com>
2026-02-23 16:26:00 -0500

    moved other files to a new repo

--- projects/Commun_Sim/src_/Scenes/Agents/resident_agent.gd
+++
@@ -1,5 +0,0 @@
-# resident agent script that will describe their unique movement and behaviour
-
-extends Base_Agent
-
-# TODO: finish implementing the resident agent script and the rest of the agents
